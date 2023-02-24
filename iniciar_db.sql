da339acf5a81fcb78a6c36664eab5e701328fd00
Doqso <p3lopubico@gmail.com>
2023-02-24 16:51:55 +0100

    actualizado para tener todo en un solo archivo

--- iniciar_db.sql
+++
@@ -1,8 +0,0 @@
-DROP SCHEMA IF EXISTS picapoint;
-CREATE SCHEMA picapoint; 
-USE picapoint;
-source picapoint_empresa.sql;
-source picapoint_usuario.sql;
-source picapoint_maquina.sql;
-source picapoint_producto.sql;
-source picapoint_maquina_has_producto.sql;
